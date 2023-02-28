fd6a26682433b2255cca88105838e60ef446c8c9
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-02-28 18:58:05 -0300

    fix: :bug: Solved the bug on the img of the menu btn.

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -7,10 +7,17 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  buttonImage: string = '../../../../assets/icons/menu.png';
   showMenu: boolean = false;
 
   toggleMenu(): void {
-    this.showMenu = !this.showMenu;
+      this.showMenu = !this.showMenu;
+    if (this.showMenu) {
+      this.buttonImage = '../../../../assets/icons/cruzar.png';
+    } else {
+      this.buttonImage = '../../../../assets/icons/menu.png';
+    }
+
   }
 
 }
