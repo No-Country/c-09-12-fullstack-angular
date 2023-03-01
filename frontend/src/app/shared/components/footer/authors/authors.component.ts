6f1a9dbd7ab8d75c2cf061826b3018f339db1955
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-03-01 17:12:12 -0300

    feat: :sparkles: Footer DONE.

--- frontend/src/app/shared/components/footer/authors/authors.component.ts
+++ frontend/src/app/shared/components/footer/authors/authors.component.ts
@@ -4,29 +4,27 @@ import { v4 as uuidv4 } from 'uuid';
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
-  styleUrls: ['./authors.component.scss']
+  styleUrls: ['./authors.component.scss'],
 })
 export class AuthorsComponent {
-
   authors: authors[] = [
     {
       id: uuidv4(),
       linkedinURL: 'https://www.linkedin.com/in/vallejosjoseluis/',
       githubURL: 'https://github.com/NikolaiKoz',
-      name: 'Jose Luis Vallejos'
+      name: 'Jose Luis Vallejos',
     },
     {
       id: uuidv4(),
       linkedinURL: 'https://www.linkedin.com/in/sharonormachea/',
       githubURL: 'https://github.com/SharonOrmachea',
-      name: 'Sharon Ormachea'
+      name: 'Sharon Ormachea',
     },
     {
       id: uuidv4(),
       linkedinURL: 'https://www.linkedin.com/in/tony-ospino-parra-276454167/',
       githubURL: 'https://github.com/TonyOP46',
-      name: 'Tony Ospino Parra'
+      name: 'Tony Ospino Parra',
     },
   ];
-
 }
