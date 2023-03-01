bf0e61dd6c61ddf1786dbae017058a1455f57a48
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-02-28 22:18:45 -0300

    feat: :art: Responsive cards done.

--- frontend/src/app/public/home/components/cards/cards.component.ts
+++ frontend/src/app/public/home/components/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { home } from '../../../../shared/interface/home';
 
 @Component({
   selector: 'app-cards',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class CardsComponent {
 
+  cards: home[] = [
+    {
+      id: 1,
+      linkTitle: 'Ir a sección de musculatura',
+      linkRouter: '/home/routine-one',
+      alt: 'Rutina de musculatura',
+      src: '../../../../../assets/cards-home/musculatura.jpg',
+      title: 'Musculatura'
+    },
+    {
+      id: 2,
+      linkTitle: 'Ir a sección de definicion',
+      linkRouter: '/home/routine-two',
+      alt: 'Rutina de definicion',
+      src: '../../../../../assets/cards-home/definicion.jpg',
+      title: 'Definicion'
+    },
+    {
+      id: 3,
+      linkTitle: 'Ir a sección de Fuerza',
+      linkRouter: '/home/routine-three',
+      alt: 'Rutina de fuerza',
+      src: '../../../../../assets/cards-home/fuerza.jpg',
+      title: 'Fuerza'
+    }
+  ];
+
 }
+
+
