55e4f9369e80b1335b1bf4f2361af24cffa7bdb4
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-03-04 20:30:12 -0300

    feat: :zap: Menu options with role DONE.

--- frontend/src/app/shared/components/navbar/menu/menu.component.ts
+++ frontend/src/app/shared/components/navbar/menu/menu.component.ts
@@ -1,20 +1,83 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent {
+  currentUserRole: string = 'coach';
 
+  menuOptions: { id: string; routerLink: string; name: string }[] = [];
 
-
-  constructor(private authService:AuthService){}
-
-  onLogout():void{
-    this.authService.logout();
+  ngOnInit() {
+    if (this.currentUserRole === 'admin') {
+      this.menuOptions = [
+        {
+          id: uuidv4(),
+          routerLink: '/profile',
+          name: 'Perfil',
+        },
+        {
+          id: uuidv4(),
+          routerLink: '/admin/clients',
+          name: 'Clientes',
+        },
+        {
+          id: uuidv4(),
+          routerLink: '/admin/coach',
+          name: 'Entrenadores',
+        },
+        {
+          id: uuidv4(),
+          routerLink: '/admin/routine',
+          name: 'Rutinas',
+        },
+        {
+          id: uuidv4(),
+          routerLink: '/admin/exercises',
+          name: 'Ejercicios',
+        },
+      ];
+    } else if (this.currentUserRole === 'coach') {
+      this.menuOptions = [
+        {
+          id: uuidv4(),
+          routerLink: '/profile',
+          name: 'Perfil',
+        },
+        {
+          id: uuidv4(),
+          routerLink: '/admin/clients',
+          name: 'Clientes',
+        },
+        {
+          id: uuidv4(),
+          routerLink: '/admin/routine',
+          name: 'Rutinas',
+        },
+      ];
+    } else if (this.currentUserRole === 'client') {
+      this.menuOptions = [
+        {
+          id: uuidv4(),
+          routerLink: '/profile',
+          name: 'Perfil',
+        },
+        {
+          id: uuidv4(),
+          routerLink: '/admin/routine',
+          name: 'Rutinas',
+        },
+      ];
+    }
   }
 
+  constructor(private authService: AuthService) {}
 
+  onLogout(): void {
+    this.authService.logout();
+  }
 }
